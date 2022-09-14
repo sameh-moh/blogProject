589b186eb3a6f00cb724b78753dd6fbbc4c9afc0
sameh-moh <sameh.mohamed@espace.com.eg>
2022-09-14 15:59:03 +0200

    viewAndEditUser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: :index
 
-    def new
-        @user = User.new
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
     end
 
     def show
         @user = User.find(params[:id])
     end
 
+    
     private
     def user_params
       params.require(:user).permit(:name, :email, :password)
